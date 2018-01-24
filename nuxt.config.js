f873f6be8865a3664f28e3f0f3b62a1d903bf7b2
harry patel <harry@unomee.com>
2018-01-24 14:16:07 +0000

    lighthouse pwa failed audit - does not redirect to https

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     loading: { color: "#3B8070" },
     router: {
-        middleware: ["setEnv"]
+        middleware: ["setEnv","https"]
     },
     css: [
         "assets/styles/style.css"
