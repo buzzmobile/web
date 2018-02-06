20cfaeae6b0f39b90f2cb82deaea736dddf5f86d
harry patel <harry@unomee.com>
2018-02-06 14:30:24 +0000

    removed bootstrap as an yarn install as we're using the file based version for now. added vuex-router-sync as were really routing now

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,9 +38,8 @@ module.exports = {
         // ["@nuxtjs/localtunnel", { subdomain: "buzzmobile" }], //https://buzzmobile.localtunnel.me/
         "@nuxtjs/pwa",
         // Doc: https://github.com/nuxt-community/axios-module#usage
-        "@nuxtjs/axios"//,
-    // Doc: https://bootstrap-vue.js.org/docs/
-    //"bootstrap-vue/nuxt"
+        "@nuxtjs/axios",
+        "nuxt-vuex-router-sync"
     ],
     axios: {
     // See https://github.com/nuxt-community/axios-module#options
