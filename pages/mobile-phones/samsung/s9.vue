08771de6a1c24dd99e7bb6dd95242347833aef02
harry patel <harry@unomee.com>
2018-03-05 20:34:02 +0000

    added missing alt tags

--- pages/mobile-phones/samsung/s9.vue
+++ pages/mobile-phones/samsung/s9.vue
@@ -17,21 +17,21 @@
 ~/assets/images/galaxy-s9-black-back-480.jpg 480w,
 ~/assets/images/galaxy-s9-black-back-684.jpg 684w,
 ~/assets/images/galaxy-s9-black-back-763.jpg 763w,
-~/assets/images/galaxy-s9-black-back-900.jpg 900w" src="~/assets/images/galaxy-s9-black-back-360.jpg" alt="">
+~/assets/images/galaxy-s9-black-back-900.jpg 900w" src="~/assets/images/galaxy-s9-black-back-360.jpg" alt="Samsung S9 in Midnight Black">
             </div>
             <div class="col-xs-4">
               <img class="img-responsive" sizes="30vw" srcset="
 ~/assets/images/galaxy-s9-purple-back-360.jpg 360w,
 ~/assets/images/galaxy-s9-purple-back-581.jpg 581w,
 ~/assets/images/galaxy-s9-purple-back-785.jpg 785w,
-~/assets/images/galaxy-s9-purple-back-900.jpg 900w" src="~/assets/images/galaxy-s9-purple-back-360.jpg" alt="">
+~/assets/images/galaxy-s9-purple-back-900.jpg 900w" src="~/assets/images/galaxy-s9-purple-back-360.jpg" alt="Samsung S9 in Lilac Purple">
             </div>
             <div class="col-xs-4">
               <img class="img-responsive" sizes="30vw" srcset="
 ~/assets/images/galaxy-s9-blue-back-360.jpg 360w,
 ~/assets/images/galaxy-s9-blue-back-596.jpg 596w,
 ~/assets/images/galaxy-s9-blue-back-797.jpg 797w,
-~/assets/images/galaxy-s9-blue-back-900.jpg 900w" src="~/assets/images/galaxy-s9-blue-back-360.jpg" alt="">
+~/assets/images/galaxy-s9-blue-back-900.jpg 900w" src="~/assets/images/galaxy-s9-blue-back-360.jpg" alt="Samsung S9 in Coral Blue">
             </div>
           </div>
         </div>
