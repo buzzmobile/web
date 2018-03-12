7f874f59f8a267ad1ca69c53059e7cd926c53600
harry patel <harry@unomee.com>
2018-03-12 22:27:51 +0000

    tidy up

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,7 @@
 <template>
-  <div>
-    <section >
-      <div >
-        <div >
-          <div >
-            <div >
-              <h1></h1>
-              <div >
-                <div >
-                  <nuxt-link to="/mobile-phones/apple/iPhoneX">Apple iPhone X</nuxt-link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div >
-            <div >
-              <h1></h1>
-              <div >
-                <div >
-                  <nuxt-link to="/mobile-phones/samsung/s9">Samsung Galaxy S9/+</nuxt-link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+  <div class="flex flex-col items-center">
+    <nuxt-link to="/mobile-phones/apple/iPhoneX">Apple iPhone X</nuxt-link>
+    <nuxt-link to="/mobile-phones/samsung/s9">Samsung Galaxy S9/+</nuxt-link>
   </div>
 </template>
+
