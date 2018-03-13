e94371a880e00dd47d8358a08cdb7c6aeba3e847
harry patel <harry@unomee.com>
2018-03-13 20:26:40 +0000

    max width for design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <header class="font-sans antialiased" id="app">
-      <nav class="flex items-center justify-between flex-wrap bg-brand-grey p-4">
-        <div class="flex items-center flex-no-shrink mr-6">
-          <nuxt-link class="no-underline font-bold text-xl tracking-wide text-brand-pink" to="/">Mopho</nuxt-link>
-        </div>
-        <div class="block sm:hidden">
-          <button @click="menuToggled" class="flex items-center px-3 py-2 border rounded text-pink-light border-brand-pink hover:text-pink-light hover:border-pink-light">
-            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-          <div class="text-sm sm:flex-grow">
-            <nuxt-link to="/mobile-phones/apple/iphonex" class="no-underline block text-brand-pink font-medium hover:text-pink-light sm:inline-block mt-4 mr-4 sm:mt-0">
-              iPhone X
-            </nuxt-link>
-            <nuxt-link to="/mobile-phones/samsung/s9" class="no-underline block text-brand-pink font-medium hover:text-pink-light sm:inline-block mt-4 mr-4 sm:mt-0">
-              Samsung S9/+
-            </nuxt-link>
+  <div>
+    <div class="flex flex-col min-h-screen no-bigger-than">
+      <header class="font-sans antialiased" id="app">
+        <nav class="flex items-center justify-between flex-wrap bg-brand-grey p-4">
+          <div class="flex items-center flex-no-shrink mr-6">
+            <nuxt-link class="no-underline font-bold text-xl tracking-wide text-brand-pink" to="/">Mopho</nuxt-link>
           </div>
-        </div>
-      </nav>
-    </header>
-    <main class="flex-1">
-      <nuxt/>
-    </main>
-    <footer class="bg-brand-grey text-brand-pink font-light w-full p-4">
-      <p class="text-center">mopho.com 2018</p>
-    </footer>
+          <div class="block sm:hidden">
+            <button @click="menuToggled" class="flex items-center px-3 py-2 border rounded text-pink-light border-brand-pink hover:text-pink-light hover:border-pink-light">
+              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+            <div class="text-sm sm:flex-grow">
+              <nuxt-link to="/mobile-phones/apple/iphonex" class="no-underline block text-brand-pink font-medium hover:text-pink-light sm:inline-block mt-4 mr-4 sm:mt-0">
+                iPhone X
+              </nuxt-link>
+              <nuxt-link to="/mobile-phones/samsung/s9" class="no-underline block text-brand-pink font-medium hover:text-pink-light sm:inline-block mt-4 mr-4 sm:mt-0">
+                Samsung S9/+
+              </nuxt-link>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main class="flex-1">
+        <nuxt/>
+      </main>
+      <footer class="bg-brand-grey text-brand-pink font-light w-full p-4">
+        <p class="text-center">mopho.com 2018</p>
+      </footer>
+    </div>
   </div>
+
 </template>
+
 
 
 
