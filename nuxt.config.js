b7a69369b74ec4b67b1c458b112100212a9af13b
harry patel <harry@unomee.com>
2018-03-14 21:34:01 +0000

    link on paste in slack is wrong so hoping this will fix it

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,11 @@ module.exports = {
     mode: "universal",
     // Headers of the page
     head: {
-        title: pkg.name,
+        title: "Mopho",
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: pkg.description },
+            { hid: "description", name: "description", content: "Mobile Contracts buy Mopho" },
             { name: "http-equiv", content: "IE=edge" }
         ],
         link: [
