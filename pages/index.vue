b37b4af1a813f0d1b97703a2da52fecf038a311c
harry patel <harry@unomee.com>
2018-03-15 16:42:49 +0000

    button order swap

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <h2 class="mt-1 sm:mt-4">your next mobile</h2>
     <img class="mt-1 sm:mt-4" src="~/assets/images/home/binoculars-s9-x.jpg" alt="">
     <p class="text-lg text-center mt-2 sm:mt-4">lets simplify finding your next mobile</p>
-    <nuxt-link class="btn hover:bg-pink-light w-4/5 sm:w-3/5 sm:max-w-xs px-4 py-2 mt-4 sm:mt-8" to="/mobile-phones/apple/iPhoneX">APPLE IPHONE X</nuxt-link>
     <nuxt-link class="btn hover:bg-pink-light w-4/5 sm:w-3/5 sm:max-w-xs px-4 py-2 mt-4 sm:mt-8" to="/mobile-phones/samsung/s9">SAMSUNG GALAXY S9/+</nuxt-link>
+    <nuxt-link class="btn hover:bg-pink-light w-4/5 sm:w-3/5 sm:max-w-xs px-4 py-2 mt-4 sm:mt-4" to="/mobile-phones/apple/iPhoneX">APPLE IPHONE X</nuxt-link>
   </div>
 </template>
 
